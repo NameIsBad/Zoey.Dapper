3bed4d420bae208632293dc6de3851672d44b09f
杜克迪 <dukedi@outlook.com>
2018-10-12 19:37:39 +0800

    add mysql

---
+++ src/Zoey.Dapper.MySql/DependencyInjection/ZoeyDapperBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using Zoey.Dapper.Abstractions;
+
+namespace Zoey.Dapper.DependencyInjection
+{
+    public static class ZoeyDapperBuilderExtensions
+    {
+        public static IZoeyDapperBuilder AddMSSQLserver(this IZoeyDapperBuilder builder, Action<ZoeyDapperDataBaseOptions> configureOptions)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (configureOptions == null)
+            {
+                throw new ArgumentNullException(nameof(configureOptions));
+            }
+            builder.Services.AddScoped<IDbConnection, MySqlConnection>();
+            builder.Services.Configure(configureOptions);
+            return builder;
+        }
+    }
+}
