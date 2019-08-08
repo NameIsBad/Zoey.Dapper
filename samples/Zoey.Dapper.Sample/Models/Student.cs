d6f03bcfa1bb013809091a6c94ab5dd5b16ad606
Zoey <453414247@qq.com>
2019-08-08 10:11:33 +0800

    整理目录
    整理目录

---
+++ samples/Zoey.Dapper.Sample/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoey.Dapper.Sample.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public float Score { get; set; }
+    }
+}
